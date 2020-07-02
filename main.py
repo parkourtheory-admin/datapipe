5a894c73986385b9c79edb6f69618aac24ea0b61
Justin Chen <ch3njus@gmail.com>
2020-07-02 17:36:46 -0400

    Refactored main

--- main.py
+++ main.py
@@ -11,6 +11,34 @@ import config
 from utils import is_config, write, clean_logs, str2bool, accuracy
 from tasks import *
 
+'''
+Parallel task execution
+
+inputs:
+pipe (list) Tasks to execute
+'''
+def parallel(pipe):
+    pipe = [mp.Process(target=t.run) for t in pipe]
+    for t in pipe: t.start()
+    for t in pipe: t.join()
+
+'''
+Sequential execution of pipeline
+
+inputs:
+pipe (list) Tasks to execute
+log  (dict) Log for failed tasks
+'''
+def sequential(pipe, log):
+    for i, t in enumerate(pipe):
+        task = type(t).__name__
+        try: 
+            t.run()
+            print(f'[{i}] {task} succeeded\n')
+        except Exception as e: 
+            print(f'[{i}] {task}.py failed\n{str(e)}\n')
+            log[type(t).__name__] = str(e)
+
 
 def main():
     parser = argparse.ArgumentParser()
@@ -29,19 +57,8 @@ def main():
         pipe.append(cl[0][1](cfg)) # index 0 is class name and index 1 is object
 
     log = {}
-    if cfg.parallel:
-        pipe = [mp.Process(target=t.run) for t in pipe]
-        for t in pipe: t.start()
-        for t in pipe: t.join()
-    else: 
-        for i, t in enumerate(pipe):
-            task = type(t).__name__
-            try: 
-                t.run()
-                print(f'[{i}] {task} succeeded\n')
-            except Exception as e: 
-                print(f'[{i}] {task}.py failed\n{str(e)}\n')
-                log[type(t).__name__] = str(e)
+    if cfg.parallel: parallel(pipe)
+    else: sequential(pipe, log)
 
     accuracy(log, pipe)
     write('datapipe.json', log)
