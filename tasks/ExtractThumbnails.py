909bd7f09f21368f0841182374bfbdc7932ae08c
Justin Chen <ch3njus@gmail.com>
2020-07-02 20:55:08 -0400

    Fixed output path

--- tasks/ExtractThumbnails.py
+++ tasks/ExtractThumbnails.py
@@ -11,13 +11,16 @@ class ExtractThumbnails(object):
         self.cfg = config
 
 
-    def run(self):
-        df = pd.read_csv(self.cfg.video_csv, header=0, sep='\t')
+    def run(self):        
         v = vid.Video()
+        
+        df = pd.read_csv(self.cfg.video_csv, header=0, sep='\t')
         res = v.extract_thumbnails(self.cfg.video_src, 300, 168)
 
         df, err = clt.update_thumbnail(df, res)
-        err.to_csv(os.path.join(self.cfg.video_csv_out, 'missing_thumbnails.tsv'), sep='\t')
+        
+        missing_dst = os.path.join(self.cfg.video_csv_out, 'missing_thumbnails.tsv')
+        updated_dst = os.path.join(self.cfg.video_csv_out, 'updated.tsv')
 
-        dst = os.path.join(self.cfg.video_csv_out, 'updated.tsv')
-        df.to_csv(self.cfg.video_csv_out, index=False, sep='\t')
+        err.to_csv(missing_dst, index=False, sep='\t')
+        df.to_csv(updated_dst, index=False, sep='\t')
