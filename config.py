ea819e39d30925e2b699114bcf4e6c9e3bbea74b
Justin Chen <ch3njus@gmail.com>
2021-02-24 13:47:48 -0500

    Added test mask

--- config.py
+++ config.py
@@ -39,9 +39,12 @@ class Configuration(object):
 
         # dataset configuration
         split = cfg['dataset']
-        self.train_split = split['train_split']
-        self.val_split = split['val_split']
-        self.test_split = split['test_split']
+        self.train_split = float(split['train_split'])
+        self.val_split = float(split['val_split'])
+        self.test_split = float(split['test_split'])
+
+        if self.train_split + self.val_split + self.test_split != 1:
+            raise Exception('data set split should sum to 1')
         
         if video['csv_out']: make_dir(video['csv_out'])
         if video['src']: make_dir(video['src'])
--- tasks/PruneGraphMask.py
+++ tasks/PruneGraphMask.py
@@ -27,28 +27,34 @@ class PruneGraphMask(object):
 		# create validation and test sets using moves that do not have videos
 		# note: this will not always be true when the video table is complete
 		df = pd.merge(moves, videos, on='id')
-		train_mask = df['embed'].notnull()
-		validation_mask = df['embed'].isnull()
+		train_mask = df['link'].notnull()
+		validation_mask = df['link'].isnull()
 		test_mask = None
 
 		test_split = self.cfg.test_split
-		val_idx = [i for i in validation_mask.tolist() if i == 1]
-		print(val_idx)
+		val_idx = [i for i, item in enumerate(validation_mask) if item]
 		test_idx = np.random.choice(val_idx, int(test_split*len(df)))
-		print(test_idx)
-		val_mask = np.array(validation_mask.tolist())
-		print(val_mask)
-		test_mask = val_mask[test_idx]
-		print(test_mask)
-		validation_mask -= test_mask
-		print(validation_mask)
+		val_mask = np.array(validation_mask.tolist(), dtype=float)
+		test_mask = np.zeros(len(val_mask))
+		test_mask[test_idx] = 1
+		val_mask -= test_mask
+		val_mask = pd.Series(val_mask, dtype=bool)
+		test_mask = pd.Series(test_mask, dtype=bool)
+
+		print(type(train_mask))
+		print(type(val_mask))
+		print(type(test_mask))
 
 		train_size = len(df[train_mask])
-		val_size = len(df[validation_mask])
-		print(f'train mask: {train_size}\tvalidation mask: {val_size}\ttotal: {train_size+val_size}')
+		val_size = len(df[val_mask])
+		test_size = len(df[test_mask])
+		total = train_size+val_size+test_size
+
+		assert total == len(df)
+		print(f'train mask: {train_size}\tvalidation mask: {val_size}\ttest mask: {test_size}\ttotal: {total}')
 	
 		train_mask.to_csv(os.path.join(self.cfg.video_csv_out, 'train_mask.tsv'), sep='\t', index=False)
-		validation_mask.to_csv(os.path.join(self.cfg.video_csv_out, 'validation_mask.tsv'), sep='\t', index=False)
+		val_mask.to_csv(os.path.join(self.cfg.video_csv_out, 'validation_mask.tsv'), sep='\t', index=False)
 		test_mask.to_csv(os.path.join(self.cfg.video_csv_out, 'test_mask.tsv'), sep='\t', index=False)
 
 
