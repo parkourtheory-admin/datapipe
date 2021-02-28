79601224e0036d0d158d354be2f4a40a6ed674d6
Justin Chen <ch3njus@gmail.com>
2021-02-28 16:28:50 -0500

    Fixed type casting
    nx.relabel_graph was converting the string names to string integer ids. Needed to add an addition nx.relabel_graph to then convert those string integers to type ints.
    
    Also added an assertion to check that the original graph is recoverable with nx.relabel_graph to check that the string to int id conversion is correct.

--- tasks/RelabelGraph.py
+++ tasks/RelabelGraph.py
@@ -19,8 +19,24 @@ class RelabelGraph(object):
 
 		re_G = None
 		with open(os.path.join(self.cfg.video_csv_out, 'adjlist.json'), 'r') as file:
-			G = nx.Graph(json.load(file)).to_undirected()
+			G = nx.Graph(json.load(file))
+
+			'''
+			convert string names to int ids. however, this first line only converts string int ids.
+				then convert the string int ids to ints
+			'''
 			re_G = nx.relabel_nodes(G, node_map)
+			re_G = nx.relabel_nodes(re_G,{i:int(i) for i in re_G.nodes()})
+
+			'''
+			invert the original dictionary, convert the int id node version back to the original strings
+			this is just for performing the assertion to check that the conversion is correct and that
+			the original graph is recoverable.
+			'''
+			undo = nx.relabel_nodes(re_G, {v:k for k,v in node_map.items()})
+
+			assert G.edges() == undo.edges()
+
 
 		with open(os.path.join(self.cfg.video_csv_out, 'adjlist.json'), 'w') as file:
 			json.dump(nx.to_dict_of_lists(re_G), file)
