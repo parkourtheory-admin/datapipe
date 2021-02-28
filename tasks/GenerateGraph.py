711388153435623a886d0cd78a7284748cf15c36
Justin Chen <ch3njus@gmail.com>
2021-02-28 15:49:03 -0500

    Added assertion
    Check that the number of nodes in the generated graph has the same number of rows as the move table.

--- tasks/GenerateGraph.py
+++ tasks/GenerateGraph.py
@@ -16,5 +16,7 @@ class GenerateGraph(object):
 		moves = pd.read_csv(self.cfg.move_csv, header=0, sep='\t')
 		G = rel.dataframe_to_graph(moves)
 
+		assert len(moves) == len(G.nodes())
+
 		with open(os.path.join(self.cfg.video_csv_out, 'adjlist.json'), 'w') as file:
 			json.dump(nx.to_dict_of_lists(G), file, ensure_ascii=False, indent=4)
